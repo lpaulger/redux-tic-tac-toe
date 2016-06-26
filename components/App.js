8536ec73b02767c7f2381a1f18241699d4109936
Lucas Paulger <lucas.paulger@gmail.com>
2016-06-26 15:35:59 +0300

    remove unrelated code

--- components/App.js
+++ components/App.js
@@ -1,13 +1,8 @@
 import React from 'react'
-import Footer from './Footer'
-import AddTodo from '../containers/AddTodo'
-import VisibleTodoList from '../containers/VisibleTodoList'
 
 const App = () => (
   <div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
+    <h1>Hello World</h1>
   </div>
 )
 
